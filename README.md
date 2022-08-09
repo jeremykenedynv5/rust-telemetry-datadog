0aca5c5fabe2ef93067febcc4ae2663067029df0
Yokesh <yokesh.thirumoorthi@nv5.com>
2022-08-09 11:06:07 -0700

    Modify LukeMathWalker opentelementry example.

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# rust-telemetry-datadog
-A prototype of Rust webserver (actix+postgres) saving traces in datadog
+# Rust-telemetry-datadog
+
+This project is a prototype to implement observervability in rust-actix-web framework. Trace are generated for each request and are stored in open-telemetry compatible services (Jaeger or Datadog).
+## Prerequisites
+
+To execute this example you need a running Jaeger instance.  
+You can launch one using Docker:
+
+```bash
+docker run -d -p6831:6831/udp -p6832:6832/udp -p16686:16686 jaegertracing/all-in-one:latest
+```
+
+## Running
+
+You can launch this example with 
+
+```bash
+cargo run
+```
+
+An `actix-web` application will be listening on port `8000`.  
+You can fire requests to it with:
+
+```bash
+curl -v http://localhost:8000/OptimusPrime
+```
+```text
+Hello OptimusPrime!
+```
+
+## Traces
+
+You can look at the exported traces in your browser by visiting [http://localhost:16686](http://localhost:16686).  
+Spans will be also printed to the console in JSON format, as structured log records.
+
+## Credits
+This application uses Open Source components. You can find the source code of their open source projects along with license information below. I acknowledge and am grateful to these developers for their contributions to open source.
+
+```
+ Project: https://github.com/LukeMathWalker/tracing-actix-web/tree/main/examples/opentelemetry
+ Copyright (c) 2022 LukeMathWalker
+ License (MIT) https://github.com/LukeMathWalker/tracing-actix-web/blob/main/LICENSE-MIT
+```
+
+## License
+
+This program is licensed under the "MIT License". Please see the file LICENSE in the source distribution of this software for license terms.
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,72 @@
+// Copyright © 2022 Yokesh Thirumoorthi
+// [This program is licensed under the "MIT License"]
+// Please see the file LICENSE in the source
+// distribution of this software for license terms.
+
+// CREDITS
+// Project: https://github.com/LukeMathWalker/tracing-actix-web/tree/main/examples/opentelemetry
+// Copyright (c) 2022 LukeMathWalker
+// License (MIT) https://github.com/LukeMathWalker/tracing-actix-web/blob/main/LICENSE-MIT
+
 use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use opentelemetry::{
+    global, runtime::TokioCurrentThread, sdk::propagation::TraceContextPropagator,
+};
+use std::io;
+use tracing_actix_web::TracingLogger;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::{EnvFilter, Registry};
 
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
     format!("Hello {}!", &name)
 }
 
-#[tokio::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+fn init_telemetry() {
+    let app_name = "tracing-actix-datadog";
+
+    // Start a new Jaeger trace pipeline.
+    // Spans are exported in batch - recommended setup for a production application.
+    global::set_text_map_propagator(TraceContextPropagator::new());
+    let tracer = opentelemetry_jaeger::new_pipeline()
+        .with_service_name(app_name)
+        .install_batch(TokioCurrentThread)
+        .expect("Failed to install OpenTelemetry tracer.");
+
+    // Filter based on level - trace, debug, info, warn, error
+    // Tunable via `RUST_LOG` env variable
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or(EnvFilter::new("info"));
+    // Create a `tracing` layer using the Jaeger tracer
+    let telemetry = tracing_opentelemetry::layer().with_tracer(tracer);
+    // Create a `tracing` layer to emit spans as structured logs to stdout
+    let formatting_layer = BunyanFormattingLayer::new(app_name.into(), std::io::stdout);
+    // Combined them all together in a `tracing` subscriber
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(telemetry)
+        .with(JsonStorageLayer)
+        .with(formatting_layer);
+    tracing::subscriber::set_global_default(subscriber)
+        .expect("Failed to install `tracing` subscriber.")
+}
+
+#[actix_web::main]
+async fn main() -> io::Result<()> {
+    init_telemetry();
+
+    HttpServer::new(move || {
         App::new()
+            .wrap(TracingLogger::default())
             .route("/", web::get().to(greet))
             .route("/{name}", web::get().to(greet))
     })
     .bind("127.0.0.1:8000")?
     .run()
-    .await
+    .await?;
+
+    // Ensure all spans have been shipped to Jaeger.
+    opentelemetry::global::shutdown_tracer_provider();
+
+    Ok(())
 }
